e34ece907d68be3e3cec5b485b2948f9d48e840e
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-10-06 13:52:51 +0530

    added locationOfExpenditure variable

--- src/assets/components/ExpenseItem.jsx
+++ src/assets/components/ExpenseItem.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2023, 10, 6);
+  const expenseTitle = "Car Insurence";
+  const expenseAmount = 567.56;
+  const locationOfExpenditure = "New Yark";
   return (
-    <div>
-      <div>October 06 2023</div>
-      <div>
-        <h2>Car Insurence</h2>
-        <div>$ 567.56</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{locationOfExpenditure}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
