e12557a2c26d071bc59aaf01fb33aa4925244185
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-10-09 15:54:55 +0530

    add expense btn for conditional randering done

--- src/assets/components/newExpense/NewExpense.jsx
+++ src/assets/components/newExpense/NewExpense.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [formOpen, setFormOpen] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
+    setFormOpen(false);
     const expenseData = { ...enteredExpenseData, id: Date.now() };
     props.onAddExpense(expenseData);
   };
 
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+  let formContent = (
+    <button onClick={() => setFormOpen(true)}>Add Expense</button>
   );
+
+  if (formOpen) {
+    formContent = <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />;
+  }
+
+  return <div className="new-expense">{formContent}</div>;
 };
 
 export default NewExpense;
